f7f0b69b5bbd223d047182f068b1e2cd267b3464
Ezell <lynellf@gmail.com>
2019-12-28 17:58:43 -0600

    define output type

--- lib/main.d.ts
+++ lib/main.d.ts
@@ -1,26 +1,50 @@
-export declare function App(): Promise<{
-    abilities: string[] | undefined;
-    baseStats: {
-        baseHP: string | null;
-        baseAtk: string | null;
-        baseDef: string | null;
-        baseSpa: string | null;
-        baseSpd: string | null;
-        baseSpeed: string | null;
-    } | undefined;
-    dexNumber: string | null;
+declare type TBST = {
+    baseHP: string;
+    baseAtk: string;
+    baseDef: string;
+    baseSpa: string;
+    baseSpd: string;
+    baseSpeed: string;
+};
+declare type TBaseStats = {
+    normal: TBST;
+    alolan: TBST | null;
+    galarian: TBST | null;
+};
+export declare type TMove = {
+    moveName: string;
+    moveAccuracy: string;
+    moveEffectPct: string;
+    movePower: string;
+    movePowerPoints: string;
+    moveType: string;
+};
+declare type TPokemon = {
+    abilities: string[];
+    baseStats: TBaseStats;
+    dexNumber: string;
+    forms: {
+        form: string;
+        types: string[];
+    }[];
     imageSrc: string;
     moves: {
-        moveAccuracy: string | null;
-        moveCategory: string | undefined;
-        moveEffectPct: string | null;
-        moveForms: (string | null)[] | undefined;
-        moveName: string | null;
-        movePower: string | null;
-        movePowerPoints: string | null;
-        moveType: string | undefined;
-    }[] | undefined;
-    name: string | null;
-    types: string[];
-}[] | undefined>;
+        [key: string]: string[];
+    };
+    name: string;
+};
+export declare type TOutput = {
+    pokemon: {
+        [key: string]: TPokemon;
+    };
+    moves: {
+        [key: string]: TMove;
+    };
+    abilities: string[];
+    _tempMoves?: {
+        [key: string]: string[];
+    };
+};
+export declare function App(): Promise<TOutput | undefined>;
+export {};
 //# sourceMappingURL=main.d.ts.map
